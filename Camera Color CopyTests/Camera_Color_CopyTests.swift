706c29a7142fab7cf61b96384656b6a9142b396c
woshiyezu <52668105+woshiyezu@users.noreply.github.com>
2020-08-31 23:39:31 +0900

    add project

---
+++ Camera Color CopyTests/Camera_Color_CopyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Camera_Color_CopyTests.swift
+//  Camera Color CopyTests
+//
+//  Created by Teruhiko Tanaka on 2020/08/31.
+//  Copyright © 2020 Teruhiko Tanaka. All rights reserved.
+//
+
+import XCTest
+@testable import Camera_Color_Copy
+
+class Camera_Color_CopyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
